fe98b31b39489a8fd24740f9805b7d2deef8fd40
wanghuilong <842667976@qq.com>
2017-12-27 19:23:57 +0800

    使用vue可用的tbox测试工具

--- tbox_main.js
+++ tbox_main.js
@@ -19,160 +19,178 @@ const strToHex=dh.strToHex
 const viewEvents=new events.EventEmitter()
 
 //tbox串口
-var portTbox=''
-var reduceParse=''
+let portTbox=''
 
 //扫码枪串口
-var portScan=''
-var portScanParse=''
-// 视图相关：
-//tbox串口
-var serial   = document.getElementById('port');
-var baudrate = document.getElementById('baudrate');
-var button_connect    = document.getElementById('connect');
-var button_disconnect = document.getElementById('disconnect');
-var button_writeTbox    = document.getElementById('writeTbox');
-var button_startCheck = document.getElementById('update')
-//扫码枪串口
-var serialScan = document.getElementById('portScan');
-var button_connectScan    = document.getElementById('connectScan');
-var button_disconnectScan = document.getElementById('disconnectScan');
-
-button_disconnect.disabled = true;
-button_disconnectScan.disabled = true;
-
+let portScan=''
+//应用app
 const App=new Vue({
-    el:'app',
+    el:'#app',
     data:{
         portList:[],
         v_tBoxCom:'',
         v_tBoxLink:false,
         v_scanCom:'',
         v_scanLink:false,
+        v_scanTboxNum:'',
+        resultNotify:'测试中输出提示相关',
+        warn:false,
+        testList1:[{
+            name:'测试CAN',
+            test:true,
+            status:'ready'//ok fail
+        },{
+            name:'测试CLOCK',
+            test:true,
+            status:'ready'//ok fail
+        },{
+            name:'测试ADC',
+            test:true,
+            status:'ready'//ok fail
+        },{
+            name:'测试FLASH',
+            test:true,
+            status:'ready'//ok fail
+        }],
+        testList2:[{
+            name:'测试FATFS',
+            test:true,
+            status:'ready'//ok fail
+        },{
+            name:'测试RTC',
+            test:true,
+            status:'ready'//ok fail
+        },{
+            name:'测试mpu6500',
+            test:true,
+            status:'ready'//ok fail
+        }]
     },
-    create(){
-        DhPort.list()
-            .then(ports=>{
-                this.portList=ports
-            })
-            .catch(err=> {
-                console.error(err);
-            });
+    created(){
+        getPostList()
     },
     methods:{
-        ale(){
-            alert(this.)
-        }
+        createConnect,
+        closeConnect,
+        createScanConnect,
+        closeScanConnect,
+        startCheck,
+        writeTbox,
+        resetTest,
+        changeTestItem(item){
+            item.test=!item.test
+        },
+
     }
 })
-app()
-function app() {
-    portList()//串口列表
-
-    button_connect.onclick    = createConnect;//tbox连接
-    button_disconnect.onclick = closeConnect;//tbox断开
-
-    button_connectScan.onclick    = createScanConnect;//扫描枪连接
-    button_disconnectScan.onclick = closeScanConnect;//扫码枪断开
-
-    button_startCheck.onclick = startCheck;//tbox自检
-    button_writeTbox.onclick = writeTbox;//写入tbox
+// 获取串口列表
+function getPostList() {
+    DhPort.list()
+        .then(ports=>{
+            App.portList=ports
+        })
+        .catch(err=> {
+            console.error(err);
+        });
 }
 //根据所选串口与扫描枪建立连接
 function createScanConnect(){
-    button_connectScan.disabled = true;
-    if(!portScan||portScan.comName!=serialScan.value){
+    let COM=App.v_scanCom
+    if(!COM){
+        App.resultNotify = '请先选择串口'
+        App.warn = true
+        return
+    }
+    if(!portScan||portScan.comName!=COM){
         // 实例化一个串口类
         portScan = new DhPort({
-            comName: serialScan.value,
+            comName: COM,
             baudRate: 9600,
             autoOpen: false
         });
         // 设置tbox编码
         portRead(portScan,ScanGunParse,'reduceProcess')
             .then(data=>{
-                document.getElementById('vcusn').value = data.toString()
+                App.v_scanTboxNum = data.toString()
+                App.resultNotify = '扫描成功输出：'+data.toString()
+                App.warn = false
             })
             .catch(err=>{
-                document.getElementById('vcusn').value = err
+                App.resultNotify = '扫描失败输出：'+err
+                App.warn = true
             })
     }
 
     portScan.open().then(msg=>{
-        console.log('扫码枪：'+msg)
-        button_connectScan.setAttribute("class", "btn-gary btn")
-        button_disconnectScan.setAttribute("class", "btn-blue btn")
-        button_disconnectScan.disabled = false;
+        App.v_scanLink = true
+        App.resultNotify = '扫码枪打开成功输出:'+msg
+        App.warn = false
     }).catch(err=>{
-            console.log('扫码枪：'+err)
-        button_connectScan.disabled = false;
-        button_connectScan.setAttribute("class", "btn-blue btn")
-        button_disconnectScan.setAttribute("class", "btn-gary btn")
+        App.resultNotify = '扫码枪打开失败输出：'+err
+        App.warn = true
     })
 }
 //带触发断开与扫描枪连接
 function closeScanConnect() {
-    button_disconnectScan.disabled = true;
     portScan.close()
         .then(function (msg) {
-            console.log('扫码枪：'+msg);
-            button_disconnectScan.setAttribute("class", "btn-gary btn")
-            button_connectScan.setAttribute("class", "btn-blue btn")
-            button_connectScan.disabled = false;
+            App.v_scanLink = false
+            App.resultNotify = '扫码枪关闭成功输出:'+msg
+            App.warn = false
         })
         .catch(function (err) {
-            console.log('扫码枪：'+err);
-            button_disconnectScan.disabled = false;
-            button_connectScan.disabled = true;
+            App.resultNotify = '扫码枪关闭失败输出：'+err
+            App.warn = true
         });
 }
 
 //根据所选串口与tbox板建立连接
 function createConnect(){
-    button_connect.disabled = true;
-    if(!portTbox||portTbox.comName!=serial.value){
+    let COM=App.v_tBoxCom
+    if(!COM){
+        App.resultNotify = '请先选择串口'
+        App.warn = true
+        return
+    }
+    if(!portTbox||portTbox.comName!=COM){
         // 实例化一个串口类
         portTbox = new DhPort({
-            comName: serial.value,
+            comName: COM,
             baudRate: 115200,
             autoOpen: false
         });
     }
     portTbox.open()
         .then(msg=>{
-            console.log('tbox：'+msg)
-            button_connect.setAttribute("class", "btn-gary btn")
-            button_disconnect.setAttribute("class", "btn-blue btn")
-            button_disconnect.disabled = false;
+            App.v_tBoxLink = true
+            App.resultNotify = 'tBox串口打开成功输出:'+msg
+            App.warn = false
         })
         .catch(err=>{
-            console.error('tbox：'+err)
-            button_connect.disabled = false;
-            button_disconnect.setAttribute("class", "btn-gary btn")
-            button_connect.setAttribute("class", "btn-blue btn")
+            App.resultNotify = 'tBox串口打开失败输出：'+err
+            App.warn = true
         })
 }
 //带触发断开与vcu板连接
 function closeConnect() {
-    button_disconnect.disabled = true;
     portTbox.close()
         .then(function (msg) {
-            console.log('tbox：'+msg);
-            button_disconnect.setAttribute("class", "btn-gary btn")
-            button_connect.setAttribute("class", "btn-blue btn")
-            button_connect.disabled = false;
+            App.v_tBoxLink = false
+            App.resultNotify = 'tBox串口关闭成功输出:'+msg
+            App.warn = false
         })
         .catch(function (err) {
-            console.log('tbox：'+err);
-            button_disconnect.disabled = false;
-            button_connect.disabled = true;
-            button_disconnect.setAttribute("class", "btn-blue btn")
-            button_connect.setAttribute("class", "btn-gary btn")
+            App.resultNotify = 'tBox串口关闭失败输出：'+err
+            App.warn = true
         });
 }
 // 开始自检
 function startCheck() {
-    styleInit()
+    if(!App.v_tBoxLink){
+        App.resultNotify = '请先连接tBox'
+        App.warn = true
+        return
+    }
     let writeCan={
         head:'ffcc',
         type:'ee',
@@ -186,64 +204,73 @@ function startCheck() {
     portWriteWithReply(portTbox,writeCan,ReduceParse,'reduceProcess',1000)
         .then(data=>{
             checkSelf(hexToNum(data.slice(0,2)))
+            App.resultNotify = 'tBox自检成功'
+            App.warn = false
         })
         .catch(err=>{
-            button_startCheck.innerHTML=err
+            App.resultNotify = 'tBox自检出现错误输出：'+err
+            App.warn = true
         })
 }
 //写tbox
 function writeTbox() {
-    let tBoxNo=document.getElementById('vcusn').value
-    if(tBoxNo.length!=14){
-        button_writeTbox.innerHTML = "请先扫描二维码";
-        setTimeout(()=>{
-            button_writeTbox.innerHTML = "写入tBox编号";
-        },1500)
-    }else {
-        let writeCan={
-            head:'ffcc',
-            type:'ee',
-            id:'1e022000',
-            data:strToHex('NrWo2yqN'),
-            len:8,
-            ch:0,
-            format:1,
-            remoteType:0
-        }
-        //解密
-        portWriteNoReply(portTbox,writeCan).catch(err=>{
-            console.error('解密失败')
-        })
-        // 写入tbox
-        let can={
-            head:'ffcc',
-            type:'ee',
-            id:'01022000',
-            len:8,
-            ch:0,
-            format:1,
-            remoteType:0
-        }
-        let tBoxCans=getWriteCans(tBoxNo,'0200000001',can)
-        //第一帧
-        portWriteNoReply(portTbox,tBoxCans[0]).catch(err=>{
-            console.error('第1帧写入失败')
-        })
-        //第2帧
-        portWriteNoReply(portTbox,tBoxCans[1]).catch(err=>{
-            console.error('第2帧写入失败')
-        })
-        //第3帧
-        portWriteWithReply(portTbox,tBoxCans[2],ReduceParse,'reduceProcess',1000)
-            .then(data=>{
-                if(hexToNum(data)==1){
-                    button_writeTbox.innerHTML='写入成功'
-                }
-            })
-            .catch(err=>{
-                button_writeTbox.innerHTML=err
-            })
+    let tBoxNo=App.v_scanTboxNum
+    if(!tBoxNo){
+        App.resultNotify = '请先扫描tBox编号'
+        App.warn = true
+        return
     }
+    let writeCan = {
+        head: 'ffcc',
+        type: 'ee',
+        id: '1e022000',
+        data: strToHex('NrWo2yqN'),
+        len: 8,
+        ch: 0,
+        format: 1,
+        remoteType: 0
+    }
+    //解密
+    portWriteNoReply(portTbox, writeCan).catch(err => {
+        App.resultNotify = '解密失败:'+err
+        App.warn = true
+        return
+    })
+    // 写入tbox
+    let can = {
+        head: 'ffcc',
+        type: 'ee',
+        id: '01022000',
+        len: 8,
+        ch: 0,
+        format: 1,
+        remoteType: 0
+    }
+    let tBoxCans = getWriteCans(tBoxNo, '0200000001', can)
+    //第一帧
+    portWriteNoReply(portTbox, tBoxCans[0]).catch(err => {
+        App.resultNotify = '第1帧写入失败:'+err
+        App.warn = true
+        return
+    })
+    //第2帧
+    portWriteNoReply(portTbox, tBoxCans[1]).catch(err => {
+        App.resultNotify = '第2帧写入失败:'+err
+        App.warn = true
+        return
+    })
+    //第3帧
+    portWriteWithReply(portTbox, tBoxCans[2], ReduceParse, 'reduceProcess', 1000)
+        .then(data => {
+            if (hexToNum(data) == 1) {
+                App.resultNotify = 'tBox写入成功'
+                App.warn = false
+            }
+        })
+        .catch(err => {
+            App.resultNotify = 'tBox写入失败:'+err
+            App.warn = true
+        })
 }
 //传入14字节的tboxNumber 封装成多个帧对象返回回调
 /*
@@ -358,55 +385,40 @@ function portRead(port,canParseClass,canParseFn){
         })
     })
 }
-// ************************************************
-function portList() {
-    // 串口初始化
-    DhPort.list()
-        .then(function (ports) {
-            let str = "";
-            ports.forEach(function(port) {
-                var option = document.createElement("option");
-                option.text = port.comName + "  " + port.locationId;
-                option.value = port.comName;
-
-                str += "\<option value=" + port.comName +  "\>" + port.comName + "\<\/option\>"
-                console.log(port)
-            });
-            serial.innerHTML = str;
-            serialScan.innerHTML = str;
-        })
-        .catch(function (err) {
-            console.error(err);
-        });
-}
 // 样式重置
-function styleInit() {
-    var can     = document.getElementById('can');
-    var clock   = document.getElementById('clock');
-    var adc     = document.getElementById('adc');
-    var flash   = document.getElementById('flash');
-    var fatfs   = document.getElementById('fatfs');
-    var rtc     = document.getElementById('rtc');
-    var mpu6500 = document.getElementById('mpu');
-    var setSn   = document.getElementById('setSn');
+function resetTest() {
+    App.testList1=[{
+        name:'测试CAN',
+        test:true,
+        status:'ready'//ok fail
+    },{
+        name:'测试CLOCK',
+        test:true,
+        status:'ready'//ok fail
+    },{
+        name:'测试ADC',
+        test:true,
+        status:'ready'//ok fail
+    },{
+        name:'测试FLASH',
+        test:true,
+        status:'ready'//ok fail
+    }]
+    App.testList2=[{
+        name:'测试FATFS',
+        test:true,
+        status:'ready'//ok fail
+    },{
+        name:'测试RTC',
+        test:true,
+        status:'ready'//ok fail
+    },{
+        name:'测试mpu6500',
+        test:true,
+        status:'ready'//ok fail
+    }]
+    App.resultNotify='自检显示已重置'
 
-    can.innerHTML = "测试";
-    can.setAttribute("class", "span1");
-    clock.innerHTML = "测试";
-    clock.setAttribute("class", "span1");
-    adc.innerHTML = "测试";
-    adc.setAttribute("class", "span1");
-    flash.innerHTML = "测试";
-    flash.setAttribute("class", "span1");
-    fatfs.innerHTML = "测试";
-    fatfs.setAttribute("class", "span1");
-    rtc.innerHTML = "测试";
-    rtc.setAttribute("class", "span1");
-    mpu6500.innerHTML = "测试";
-    mpu6500.setAttribute("class", "span1");
-    setSn.innerHTML = "未设置";
-    setSn.setAttribute("class", "span1");
-    button_writeTbox.innerHTML = "写入tBox编号";
 }
 // 自检视图更新
 function checkSelf(number,cb) {
@@ -415,95 +427,19 @@ function checkSelf(number,cb) {
         checkRes='0'+checkRes
     }
 
-    var canBuf   = checkRes[7]
-    var clockBuf = checkRes[6]
-    var adcBuf   = checkRes[5]
-    var flashBuf = checkRes[4]
-    var fatfsBuf = checkRes[3]
-    var rtcBuf   = checkRes[2]
-    var mpuBuf   = checkRes[1]
+    let canBuf   = checkRes[7]
+    let clockBuf = checkRes[6]
+    let adcBuf   = checkRes[5]
+    let flashBuf = checkRes[4]
+    let fatfsBuf = checkRes[3]
+    let rtcBuf   = checkRes[2]
+    let mpuBuf   = checkRes[1]
 
-    var can   = document.getElementById('can');
-    var clock = document.getElementById('clock');
-    var adc   = document.getElementById('adc');
-    var flash = document.getElementById('flash');
-    var fatfs = document.getElementById('fatfs');
-    var rtc   = document.getElementById('rtc');
-    var mpu   = document.getElementById('mpu');
-
-    if (canBuf == 1)
-    {
-        can.innerHTML = "正常";
-        can.setAttribute("class", "greey-color span1");
-    }
-    else
-    {
-        can.innerHTML = "异常";
-        can.setAttribute("class", "red-color span1");
-    }
-    if (clockBuf == 1)
-    {
-        clock.innerHTML = "正常";
-        clock.setAttribute("class", "greey-color span1");
-    }
-    else
-    {
-        clock.innerHTML = "异常";
-        clock.setAttribute("class", "red-color span1");
-    }
-    if (adcBuf == 1)
-    {
-        adc.innerHTML = "正常";
-        adc.setAttribute("class", "greey-color span1");
-    }
-    else
-    {
-        adc.innerHTML = "异常";
-        adc.setAttribute("class", "red-color span1");
-    }
-    if (flashBuf == 1)
-    {
-        flash.innerHTML = "正常";
-        flash.setAttribute("class", "greey-color span1");
-    }
-    else
-    {
-        flash.innerHTML = "异常";
-        flash.setAttribute("class", "red-color span1");
-    }
-    if (fatfsBuf == 1)
-    {
-        fatfs.innerHTML = "正常";
-        fatfs.setAttribute("class", "greey-color span1");
-    }
-    else
-    {
-        fatfs.innerHTML = "异常";
-        fatfs.setAttribute("class", "red-color span1");
-    }
-    rtc.innerHTML = "正常";
-    rtc.setAttribute("class", "greey-color span1");
-    // if (rtcBuf == 1)
-    // {
-    //     rtc.innerHTML = "正常";
-    //     rtc.setAttribute("class", "greey-color span1");
-    // }
-    // else
-    // {
-    //     rtc.innerHTML = "异常";
-    //     rtc.setAttribute("class", "red-color span1");
-    // }
-    mpu.innerHTML = "正常";
-    mpu.setAttribute("class", "greey-color span1");
-    // if (mpuBuf == 1)
-    // {
-    //     mpu.innerHTML = "正常";
-    //     mpu.setAttribute("class", "greey-color span1");
-    // }
-    // else
-    // {
-    //     mpu.innerHTML = "异常";
-    //     mpu.setAttribute("class", "red-color span1");
-    // }
-    cb&&cb()
+    App.testList1[0].status= canBuf==1?'ok':'fail'
+    App.testList1[1].status= clockBuf==1?'ok':'fail'
+    App.testList1[2].status= adcBuf==1?'ok':'fail'
+    App.testList1[3].status= flashBuf==1?'ok':'fail'
+    App.testList2[0].status= fatfsBuf==1?'ok':'fail'
+    App.testList2[1].status= rtcBuf==1?'ok':'fail'
+    App.testList2[2].status= mpuBuf==1?'ok':'fail'
 }
